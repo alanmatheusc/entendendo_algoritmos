d8168d5f7789a46ca23f7e138cd21f6836eb6c0a
alanmatheusc <alanmatheuscardosodoamaral@gmail.com>
2025-07-20 21:02:57 -0300

    estudos

--- pesquisa_binaria/pesquisa_binaria.js
+++ pesquisa_binaria/pesquisa_binaria.js
@@ -1,5 +1,21 @@
-function staircase(n){
-  for(let i = 1; i<= n; i++){
+lista = [1,3,5,6,7]
+
+function acharNumero(lista,numero){
+  inicio = 0
+  fim = lista.length - 1
+
+  while(inicio <= fim){
+    meio = Math.floor((inicio + fim) / 2)
+    if(lista[meio] == numero){
+      return numero
+    }
+    if(lista[meio] > numero){
+      fim = meio - 1
+    }
+    else{
+      inicio = meio  +1
+    }
   }
 }
-staircase(6)
+
+console.log(acharNumero(lista,3))
