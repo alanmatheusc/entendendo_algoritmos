49bae8cd85039e9deda58d97d563902edb50d4ce
alanmatheusc <alanmatheuscardosodoamaral@gmail.com>
2025-07-21 14:48:30 -0300

    Estudo sobre programação orientada a objeto

---
+++ estrutura_de_dados/POO - Programação Orientada a Objetos/classe.js
@@ -0,0 +1,8 @@
+class funcionario{
+  constructor(nome,dataDeNascimento){
+    this.nome = nome;
+    this.dataDeNascimento = dataDeNascimento;
+  }
+}
+
+const funcionario1 = new funcionario('Alan','07062003');
---
+++ estrutura_de_dados/POO - Programação Orientada a Objetos/objetos.js
@@ -0,0 +1,9 @@
+class funcionario{
+  constructor(nome,dataDeNascimento){
+    this.nome = nome;
+    this.dataDeNascimento = dataDeNascimento;
+  }
+}
+
+// objeto é a instancia de uma classe.
+const funcionario1 = new funcionario('Alan','07062003');
